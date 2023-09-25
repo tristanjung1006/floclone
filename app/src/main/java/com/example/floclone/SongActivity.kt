26664ab49d902fcb0733193b168055767c8ae6a4
tristanjung1006 <jjjssswww1006@gmail.com>
2023-09-26 03:25:20 +0900

    SongActivity.kt-Adding miniplayer button status change

--- app/src/main/java/com/example/floclone/SongActivity.kt
+++ app/src/main/java/com/example/floclone/SongActivity.kt
@@ -2,6 +2,7 @@ package com.example.floclone
 
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.example.floclone.databinding.ActivitySongBinding
 
@@ -13,5 +14,26 @@ class SongActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState, persistentState)
         binding = ActivitySongBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.songDownIb.setOnClickListener {
+            finish()
+        }
+        binding.songMiniplayerIv.setOnClickListener {
+            setPlayerStatus(false)
+        }
+        binding.songPauseIv.setOnClickListener {
+            setPlayerStatus(true)
+        }
+    }
+
+    // 음악이 재생중일 때와 아닐 때를 구분
+    fun setPlayerStatus(isPlaying : Boolean){
+        if (isPlaying){
+            binding.songMiniplayerIv.visibility = View.VISIBLE
+            binding.songPauseIv.visibility = View.GONE
+        }
+        else {
+            binding.songMiniplayerIv.visibility = View.GONE
+            binding.songPauseIv.visibility = View.VISIBLE
+        }
     }
 }
